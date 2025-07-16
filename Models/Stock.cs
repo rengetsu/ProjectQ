973d4bb7c328f01497c40b0905fb709e95fc38c5
Pavel Trostianko <rengetsu@mail.ru>
2025-07-16 21:57:08 +0300

    Models/Stock class added

---
+++ Models/Stock.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProjectQ.Models
+{
+    [Table("Stocks", Schema = "Yahoo")]
+    public class Stock
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Ticker { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+
+        public decimal LastPrice { get; set; }
+    }
+}
