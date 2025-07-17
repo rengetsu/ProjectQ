4faa4d159d6a4a90bfde579830fa2ec2b01e646d
Pavel Trostianko <rengetsu@mail.ru>
2025-07-17 22:26:31 +0300

    Add Model for Portfolio Entry

---
+++ Models/PortfolioEntry.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProjectQ.Models
+{
+    [Table("Portfolio", Schema = "Revolut")]
+    public class PortfolioEntry
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Ticker { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+
+        [Required]
+        public decimal AvgBuyPrice { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DateAdded { get; set; }
+    }
+}
