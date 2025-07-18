aa7b56910bd10a97a55d7f1802382f8095850b23
Pavel Trostianko <rengetsu@mail.ru>
2025-07-18 22:48:56 +0300

    Create Add button to portfolio

--- Pages/Portfolio.cshtml.cs
+++ Pages/Portfolio.cshtml.cs
@@ -1,6 +1,7 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using ProjectQ.Models;
 using ProjectQ.Data;
+using ProjectQ.Models;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -17,11 +18,27 @@ namespace ProjectQ.Pages
 
         public IList<PortfolioEntry> Holdings { get; set; }
 
+        [BindProperty]
+        public PortfolioEntry NewTrade { get; set; }
+
         public void OnGet()
         {
             Holdings = _context.Portfolio
                 .OrderByDescending(p => p.DateAdded)
                 .ToList();
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid || string.IsNullOrWhiteSpace(NewTrade.Ticker))
+                return Page();
+
+            NewTrade.DateAdded = DateTime.Now;
+
+            _context.Portfolio.Add(NewTrade);
+            _context.SaveChanges();
+
+            return RedirectToPage();
         }
     }
 }
