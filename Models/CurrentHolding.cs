defb504545d1c8bbd1089d727ae2b6eed469c012
Pavel Trostianko <rengetsu@mail.ru>
2025-07-20 19:32:39 +0300

    Add New Widget to Home Page: Porfolio Allocation

---
+++ Models/CurrentHolding.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProjectQ.Models
+{
+    [Table("CurrentHoldings", Schema = "Revolut")]
+    public class CurrentHolding
+    {
+        public string Ticker { get; set; }
+        public int NetQuantity { get; set; }
+    }
+}
--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,20 +1,38 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using ProjectQ.Data;
+using ProjectQ.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace ProjectQ.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly AppDbContext _context;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, AppDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public void OnGet()
-        {
+        // Property to expose data to Razor page
+        public List<CurrentHolding> HoldingsPieData { get; set; } = new List<CurrentHolding>();
 
+        public async Task OnGetAsync()
+        {
+            try
+            {
+                HoldingsPieData = await _context.Set<CurrentHolding>().ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Failed to load current holdings for pie chart.");
+            }
         }
     }
 }
