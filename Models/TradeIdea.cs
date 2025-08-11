6f13d6e10d2465acab1977cb1952fc8ae361983c
Pavel Trostianko <rengetsu@mail.ru>
2025-08-11 23:54:01 +0300

    Create TradeIdea model

---
+++ Models/TradeIdea.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjectQ.Models
+{
+    public class TradeIdea
+    {
+        public int Id { get; set; }
+
+        [MaxLength(50)]
+        public string LongTicker { get; set; }
+
+        [MaxLength(50)]
+        public string ShortTicker { get; set; }
+
+        [MaxLength(1000)]
+        public string Description { get; set; }
+
+        public DateTime DateAdded { get; set; } = DateTime.UtcNow;
+    }
+}
