2a778482dd46113d0c49c18012ca832dab89a7ae
Pavel Trostianko <rengetsu@mail.ru>
2025-08-13 22:05:30 +0300

    Perfomance razor page created

---
+++ Pages/Perfomance.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ProjectQ.Pages
+{
+    public class PerformanceModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
